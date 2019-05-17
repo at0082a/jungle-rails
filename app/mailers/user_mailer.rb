7ae1d0f5064fbd13067191cec5737ee62e029bce
alex <you@example.com>
2019-05-17 19:00:35 +0000

    added an e-mail receipt feature upon order completion

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+
+    default from: 'no-reply@jungle.com'
+ 
+    def welcome_email(user)
+        @user = user
+        mail(to: @user.email, subject: 'Thank you for your purchase')
+    end
+
 end
