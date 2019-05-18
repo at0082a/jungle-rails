df85c12f9e265e52c34fb8b52366898e19a03878
alex <you@example.com>
2019-05-18 03:03:43 +0000

    create create function for new reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,14 @@
 class ReviewsController < ApplicationController
     def create
-        raise "Yay, I'm here!"
+      @product = Product.find(params[:product_id])
+      @review = @product.reviews.build
+
+      if @review.save
+        ;flash[:notice] = 'Review was successfully created.'
+        redirect_to @product
+        else
+        flash[:notice] = "Error creating review: #{@review.errors}"
+        redirect_to @product
       end
+   end
 end
